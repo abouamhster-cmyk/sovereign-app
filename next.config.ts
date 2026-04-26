c2c155f20d2323f36e7fc7fa94c6870610bd96bf
abouamhster-cmyk <abouamhster@gmail.com>
2026-04-26 13:44:23 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",  // ← COMMENTE CETTE LIGNE pour l'instant
   images: {
     unoptimized: true,
   },
