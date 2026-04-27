dae8ff46820501a0a3915a1059cdbab28f4827ac
abouamhster-cmyk <abouamhster@gmail.com>
2026-04-27 08:47:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -345,10 +345,6 @@ export default function DashboardPage() {
   const formatCurrency = (val: number) => {
     return new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'XOF', maximumFractionDigits: 0 }).format(val);
   };
-
-  if (isLoading) {
-  return <LoadingSpinner />;
-}
   
   return (
     <div className="p-6 lg:p-8 h-full flex flex-col overflow-y-auto bg-midnight">
